7f89dc150ec5f2afefa2df3b5db6a8d5a92646c4
Sam Chan <shaughn.chan@gmail.com>
2026-02-18 15:12:09 -0500

    updated BaseSemanticVisitor and associated classes

--- src/compiler/semantics.cpp
+++ src/compiler/semantics.cpp
@@ -1,2 +1,49 @@
 #include "compiler/semantics.hpp"
+#include <memory>
 
+using namespace ecc::compiler;
+
+void SymbolTable::push_scope(Symbol *assoc) {
+    /*
+    Create a new scope, push it onto the current one, replace current scope with
+    the new one
+    */
+
+    Box<Scope> newscope = std::make_unique<Scope>(assoc);
+
+    Scope *new_current = newscope.get();
+
+    current->inners.push_back(std::move(newscope));
+
+    current = new_current;
+}
+
+void SymbolTable::pop_scope() {
+    if (current != global.get()) {
+        if (current->outer) {
+            current = current->outer;
+        } else {
+            // there is no outer scope and we are not global,
+            // throw exception
+
+            // todo
+        }
+    }
+}
+
+Symbol *SymbolTable::lookup(std::string sym) {
+    /*
+    Look up symbol in current scope. If not there, check outer.
+    Repeat until current scope is global. Return null if not found
+    in global.
+    */
+    return nullptr; // todo
+}
+
+void SymbolTable::insert(std::string name, Box<Symbol> sym) {
+    current->symbols.insert({name, std::move(sym)});
+}
+
+BaseSemanticVisitor::ScopeGuard BaseSemanticVisitor::enter_scope(Symbol *assoc) {
+    return ScopeGuard(syms, assoc);
+}
--- src/compiler/semantics.hpp
+++ src/compiler/semantics.hpp
@@ -32,26 +32,104 @@ public:
 };
 
 /*
-The symbol table, storing all symbols in a program.
+The symbol table, storing all symbols in a given translation unit.
 */
 class SymbolTable {
 public:
+    class Scope {
+    public:
+        Scope(Symbol *assoc) : outer(nullptr), assoc(assoc), symbols(), inners() {}
+        // the outer scope enclosing the inner scope.
+        Scope *outer;
+        // a symbol associated with this scope, usually a function.
+        // if null, this is an anonymous scope.
+        Symbol *assoc;
+        // the symbol table.
+        std::map<std::string, Box<Symbol>> symbols = {};
+        // inner scopes contained within this scope.
+        Vec<Box<Scope>> inners;
+    }; // class Scope
 
-private:
-    std::map<std::string, Symbol> table = {};
+    SymbolTable() : global(std::make_unique<Scope>(nullptr)), current(global.get()) {}
+
+    // The global scope.
+    Box<Scope> global;
+    // The current scope.
+    Scope *current;
+    // Create and enter a new scope.
+    void push_scope(Symbol *assoc = nullptr);
+    // Exit the current scope to the outer one.
+    void pop_scope();
+    // Lookup a symbol by name.
+    Symbol *lookup(std::string sym);
+    // Add a new symbol to the current scope.
+    void insert(std::string name, Box<Symbol> sym);
+
+};
+
+
+/*
+The base semantic walker class that handles scoping and AST walking.
+
+The BaseSemanticVisitor class handles scoping within a SymbolTable and the basic
+AST walking operations, overriding all `visit()` member functions in the abstract
+`ast::ASTVisitor` base class. As such, a BaseSemanticVisitor simply walks the AST
+without doing anything on the nodes, pushing and popping scopes as necessary.
+
+Each class implementing a semantic validation pass should inherit from this class.
+overriding the `visit()` member functions that concern them.
+*/
+class BaseSemanticVisitor : public ast::ASTVisitor {
+public:
+    /*
+    An RAII wrapper for automatically pushing and popping scopes on a symbol table.
+
+    When a ScopeGuard is created, it is supplied with a reference to a symbol table.
+    The ScopeGuard pushes a new scope into the symbol table.
+
+    When the destructor is called, the ScopeGuard automatically pops the scope from its
+    stored symbol table reference.
+    */
+    class ScopeGuard {
+    public:
+        ScopeGuard(SymbolTable& st, Symbol *assoc) : st(st) {
+            st.push_scope(assoc);
+        }
+    
+        ~ScopeGuard() {
+            st.pop_scope();
+        }
+    
+        ScopeGuard(const ScopeGuard&) = delete;
+        ScopeGuard& operator=(const ScopeGuard&) = delete;
+    private:
+        SymbolTable& st;
+    }; // class ScopeGuard
+
+    BaseSemanticVisitor(SymbolTable& syms, types::TypeContext& types)
+    : syms(syms), types(types) {}
+
+    // A reference to a symbol table.
+    SymbolTable& syms;
+    types::TypeContext& types;
+
+    // Create and enter a new scope, supplying an associated symbol if needed.
+    ScopeGuard enter_scope(Symbol *sym = nullptr);
+
+    // todo: override all ASTVisitor methods
 };
 
 /*
 The class that performs the elaboration pass.
 */
-class Elaborator : public ast::ASTVisitor {
+class Elaborator : public BaseSemanticVisitor {
 
 };
 
 /*
 The class that performs the validation pass.
 */
-class Validator : public ast::ASTVisitor {
+class Validator : public BaseSemanticVisitor {
 
 };
 
