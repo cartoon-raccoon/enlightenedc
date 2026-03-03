372318e9e8bf2550103dd88c0da0adc336323909
cryptodire <turtlejade7@gmail.com>
2026-03-03 01:40:30 -0500

    Add gcc preprocessing

--- src/frontend/frontend.cpp
+++ src/frontend/frontend.cpp
@@ -2,33 +2,41 @@
 #include "ast/printer.hpp"
 #include "frontend/lexer.hpp"
 #include "parser.hpp"
-#include <fstream>
+#include "preproc/preproc.hpp"
+#include <sstream>
 
 using namespace ecc::frontend;
 
-int Frontend::parse(const std::string &filename) {
+int Frontend::parse(const std::string& filename) {
     file = filename;
 
-    std::ifstream input(filename);
-    if (!input.is_open()) {
-        std::cerr << "Failed to open file: " << filename << "\n";
+    try {
+        ecc::preproc::PreProcessor preproc;
+        std::string preprocessed = preproc.run(filename);
+
+        std::cout << preprocessed << "\n\n\n";
+        std::istringstream input(preprocessed);
+
+
+        ecc::ast::Program ast_root;
+
+        ecc::frontend::Lexer lexer(&input);
+        ecc::parser::Parser parser(lexer, ast_root);
+
+        // temp for testing
+        parser.set_debug_level(1);
+
+        int result = parser.parse();
+
+        if (result == 0) {
+            ecc::ast::ASTPrinter printer;
+            ast_root.accept(printer);
+        }
+
+        return result;
+
+    } catch (const std::exception& e) {
+        std::cerr << "Preprocessor error: " << e.what() << "\n";
         return 1;
     }
-
-    ecc::ast::Program ast_root;
-
-    ecc::frontend::Lexer lexer(&input);
-    ecc::parser::Parser parser(lexer, ast_root);
-
-    // temp for testing
-    parser.set_debug_level(1);
-
-    int result = parser.parse();
-
-    if (result == 0) {
-        ecc::ast::ASTPrinter printer;
-        ast_root.accept(printer);
-    }
-
-    return result;
 }
