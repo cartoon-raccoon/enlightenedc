2030c7a3149d9df9c45b14b5d4737fff4469f6ee
Sam Chan <shaughn.chan@gmail.com>
2026-03-30 18:43:45 -0400

    added helper functions to location and hash structs

--- include/util.hpp
+++ include/util.hpp
@@ -9,6 +9,7 @@
 #include <variant>
 #include <type_traits>
 #include <source_location>
+#include <functional>
 
 #ifndef NDEBUG
 #include <iostream>
@@ -56,6 +57,23 @@ using Optional = std::optional<T>;
 template<class... Ts> struct match : Ts... { using Ts::operator()...; };
 template<class... Ts> match(Ts...) -> match<Ts...>;
 
+template <typename ... Types>
+struct VarHash {
+    // Helper to combine an individual seed with a new value
+    void hash_combine(std::size_t& seed, const auto& v) const {
+        std::hash<std::decay_t<decltype(v)>> hasher;
+        // The Boost "Golden Ratio" formula
+        seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+
+    std::size_t operator()(const Types&... args) const {
+        std::size_t seed = 0;
+        // C++17 Fold Expression: applies hash_combine to every argument in args
+        (hash_combine(seed, args), ...);
+        return seed;
+    }
+};
+
 /*
 A basic point in a source file.
 */
@@ -71,6 +89,10 @@ public:
     /// Construct an empty Point (no filename, starts at 1:1).
     Point() : column(1), line(1), filename(nullptr) {}
 
+    inline bool empty() {
+        return column == 1 && line == 1 && filename == nullptr;
+    }
+
     /// Add `rhs` columns.
     inline Point&
     operator+= (int rhs) {
@@ -82,6 +104,14 @@ public:
     inline Point
     operator+ (int rhs) {
         return Point(filename, column + rhs, line);
+    }
+
+    inline bool
+    operator== (Point& other) const {
+        return
+            (*filename == *other.filename) &&
+            (column == other.column) &&
+            (line == other.line);
     }
 
     void lines(int count = 1) {
@@ -138,6 +168,52 @@ public:
     void lines(int count = 1) {
         end.lines(count);
     }
+
+    /**
+    Check if a Location crosses a file boundary.
+    */
+    inline bool crosses_files() {
+        return begin.filename != end.filename;
+    }
+
+    inline bool empty() {
+        return begin.empty() && end.empty();
+    }
+
+    /**
+    Check if `other` overlaps with `this`.
+    */
+    inline bool 
+    operator||(Location& other) const {
+        if (((begin.filename != other.begin.filename) || (end.filename != other.end.filename))) {
+            return false;
+        }
+
+        // overlap: begin.line <= other.begin.line <= other.end
+
+        return false; // todo
+    }
+
+    /**
+    Check if `other` is fully contained within `this`.
+    
+    Note that unlike `||`, this is not symmetric;
+    `other` being fully contained within `this` does not imply
+    `this` is fully contained within `other`.
+    */
+    inline bool 
+    operator&&(Location& other) const {
+        if (((begin.filename != other.begin.filename) || (end.filename != other.end.filename))) {
+            return false;
+        }
+
+        return false; // todo
+    }
+
+    inline Location
+    operator&(Location& rhs) const {
+        return Location {}; // todo
+    }
 };
 
 /** \brief Intercept output stream redirection.
@@ -171,6 +247,28 @@ struct is_variant_member<T, std::variant<Types...>>
 // Concept to check if a type T is a member of a std::variant,
 template <typename T, typename Variant>
 concept VariantMember = is_variant_member<T, Variant>::value;
+
+}
+
+// Injecting hash specializations into std namespace
+namespace std {
+
+template<>
+struct hash<ecc::util::Point> {
+    size_t operator() (const ecc::util::Point& pt) {
+        ecc::util::VarHash<std::string, int, int> hasher;
+        return hasher(*pt.filename, pt.line, pt.column);
+    }
+};
+
+
+template<>
+struct hash<ecc::util::Location> {
+    size_t operator() (const ecc::util::Location& loc) {
+        ecc::util::VarHash<ecc::util::Point, ecc::util::Point> hasher;
+        return hasher(loc.begin, loc.end);
+    }
+};
 
 }
 
