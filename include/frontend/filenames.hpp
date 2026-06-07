bff931c20c217aea451647e454e5c3732c9d7d59
Sam Chan <shaughn.chan@gmail.com>
2026-06-07 16:58:23 -0700

    started on inputfile

--- include/frontend/filenames.hpp
+++ include/frontend/filenames.hpp
@@ -3,6 +3,7 @@
 #ifndef ECC_FILENAMES_H
 #define ECC_FILENAMES_H
 
+#include <cstdint>
 #include <string>
 #include <unordered_set>
 
@@ -17,6 +18,28 @@ public:
 
 private:
     std::unordered_set<std::string> pool;
+};
+
+class InputFile {
+public:
+
+    enum class FileType : uint8_t {
+        CODE,
+        LLVMIR,
+        LLVMBC,
+        ASM,
+        OBJECT,
+        UNKNOWN,
+    };
+
+    InputFile(std::string& filename):
+        filename(&filename), filetype(filetype_from_ext(filename)) {}
+
+    std::string *filename;
+
+    FileType filetype;
+
+    static FileType filetype_from_ext(const std::string& ext);
 };
 
 } // namespace ecc::frontend
--- src/frontend/filenames.cpp
+++ src/frontend/filenames.cpp
@@ -12,3 +12,19 @@ const std::string *FilenamePool::intern(const char *str) {
     // Return the address of the string inside the set
     return &(*result.first);
 }
+
+InputFile::FileType InputFile::filetype_from_ext(const std::string& ext)  {
+    if (ext.ends_with("ec")) {
+        return FileType::CODE;
+    } else if (ext.ends_with("ll")) {
+        return FileType::LLVMIR;
+    } else if (ext.ends_with("bc")) {
+        return FileType::LLVMBC;
+    } else if (ext.ends_with("S") || ext.ends_with("s") || ext.ends_with("asm")) {
+        return FileType::ASM;
+    } else if (ext.ends_with("o")) {
+        return FileType::OBJECT;
+    } else {
+        return FileType::UNKNOWN;
+    }
+}
