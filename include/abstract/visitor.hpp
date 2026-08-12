8e8bf035cdcfa3bd8ecc256121b8790eddb0b918
Sam Chan <shaughn.chan@gmail.com>
2026-08-12 21:08:56 +0800

    added some explanatory comments

--- include/abstract/visitor.hpp
+++ include/abstract/visitor.hpp
@@ -3,12 +3,20 @@
 #ifndef ECC_ABSTRACT_VISITOR_H
 #define ECC_ABSTRACT_VISITOR_H
 
+namespace ecc {
+
 template <typename DerivedT>
 class VisitResult {};
 
 template <typename DerivedT>
 class VisitArg {};
 
+/**
+A CRTP mixin interposer that overrides the accept() function on each Node,
+avoiding repetition of `void accept()` implementations per node.
+
+See ast.hpp for usage.
+*/
 template <typename DerivedT, typename BaseT, typename VisitorT>
 class Visitable : public BaseT {
 public:
@@ -17,6 +25,9 @@ public:
     void accept(VisitorT& visitor) override { return visitor.visit(static_cast<DerivedT&>(*this)); }
 };
 
+/**
+A visitor to a single Node.
+*/
 template <typename NodeT>
 class SingleVisitor {
 public:
@@ -24,10 +35,15 @@ public:
     virtual void visit(NodeT& node) = 0;
 };
 
+/**
+An abstract CRTP class that all visitors inherit from.
+*/
 template <typename DerivedT, typename... Visitables>
 class Visitor : public SingleVisitor<Visitables>... {
 public:
     using SingleVisitor<Visitables>::visit...;
 };
+
+} // end namespace ecc
 
 #endif
