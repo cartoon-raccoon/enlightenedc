a3c6ee61e51a97c1344a84674836300da608936c
Sam Chan <shaughn.chan@gmail.com>
2026-09-13 21:42:17 +0200

    started on scoped lexer hack

--- include/frontend/lexer.hpp
+++ include/frontend/lexer.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "util/string.hpp"
 #ifndef ECC_LEXER_H
 #define ECC_LEXER_H
 
@@ -13,8 +12,31 @@
 
 #include "parser.hpp"
 #include "prelude.hpp"
+#include "util/string.hpp"
 
 namespace ecc::frontend {
+
+/**
+A class implementing the bodgiest of bodges.
+
+Look upon this and despair, the personification of jank, the bodge to rule all bodges.
+It binds the parser and lexer thus in a circularly-dependent cycle of non-context-free glory,
+forever unwillingly bound to serve one another, an ouroboros of tokens.
+
+IN TECHNICAL SPEAK: See [this](https://en.wikipedia.org/wiki/Lexer_hack) for an explanation.
+*/
+class LexerHack {
+    Vec<StringHashSet> stack;
+
+public:
+    void push_scope() {
+        stack.emplace_back();
+    }
+
+    void pop_scope() {
+        stack.pop_back();
+    }
+};
 
 class Lexer : public yyFlexLexer {
     Location loc;
